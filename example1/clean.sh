ecdd886a3c8d4a26e372ebf3544ebc31f70e6c85
Rahul Garg <rahul.garg@neewee.ai>
2020-04-26 11:54:30 +0530

    example1 added
    This contains multiple file c project compiled and linked with gcc commands in shell file

---
+++ example1/clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#########################################################################
+# File Name: clean.sh
+# Author: shift
+# mail: open_shift@163.com
+# Created Time: 2015年03月03日 星期二 21时30分34秒
+#########################################################################
+
+#delete cacu
+rm -f cacu
+
+#delelte *.o
+rm -rf add/*.o sub/*.o *.o
+
+#delete tags
+rm -f tags
